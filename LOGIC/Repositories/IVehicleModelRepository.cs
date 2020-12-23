c46571a047705fd57d6070107f39b3c5d0933c39
Gabriela1719 <44815532+Gabriela1719@users.noreply.github.com>
2020-12-23 06:56:28 +0100

    Update IVehicleModelRepository.cs

--- LOGIC/Repositories/IVehicleModelRepository.cs
+++ LOGIC/Repositories/IVehicleModelRepository.cs
@@ -2,17 +2,17 @@ using DAL.Models;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-
+using System.Threading.Tasks;
 
 namespace LOGIC
 {
     public interface IVehicleModelRepository
     {
-        IEnumerable GetVehicleModles();        
-        VehicleModel GetVehicleModelByID(int ID);        
-        void InsertVehicleModel(VehicleModel vehicleModel);        
-        void DeleteVehicleModel(int ID);        
-        void UpdateVehicleModel(VehicleModel vehicleModel);        
-        void Save(); 
+        IEnumerable<VehicleModel> GetVehicleModles();
+        Task<VehicleModel> GetVehicleModelByID(int ID);
+        Task<VehicleModel> InsertVehicleModel(VehicleModel vehicleModel);
+        Task<VehicleModel> DeleteVehicleModel(int ID);
+        Task<VehicleModel> UpdateVehicleModel(VehicleModel vehicleModel);
+        Task<VehicleModel> GetVehicleModelByID(int? id);
     }
 }
