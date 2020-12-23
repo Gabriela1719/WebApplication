5f5d9fe998668478bb10596f8d5128d8d46ca25e
Gabriela1719 <44815532+Gabriela1719@users.noreply.github.com>
2020-12-23 06:57:04 +0100

    Update VehicleModelRepository.cs

--- LOGIC/Repositories/VehicleModelRepository.cs
+++ LOGIC/Repositories/VehicleModelRepository.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using DAL.DataContext;
 using DAL.Models;
 using Microsoft.EntityFrameworkCore;
@@ -15,31 +16,39 @@ namespace LOGIC
         {
             this.context=context;
         }
-        public IEnumerable GetVehicleModles()
+        public IEnumerable<VehicleModel> GetVehicleModles()
         {
             return context.VehicleModels.ToList();
         }
-        public VehicleModel GetVehicleModelByID(int ID)
+        public async Task<VehicleModel>GetVehicleModelByID(int ID)
         {
-            return context.VehicleModels.Find(ID);
+            return await context.VehicleModels.FindAsync(ID);
         }
-        public void InsertVehicleModel(VehicleModel vehicleModel)
+        public async Task<VehicleModel> InsertVehicleModel(VehicleModel vehicleModel)
         {
-            context.VehicleModels.Add(vehicleModel);
+            var result = await context.VehicleModels.AddAsync(vehicleModel);
+            return result.Entity;
         }
-        public void DeleteVehicleModel(int ID)
+        public async Task<VehicleModel> DeleteVehicleModel(int ID)
         {
-            VehicleModel vehicleModel = context.VehicleModels.Find(ID);
-            context.VehicleModels.Remove(vehicleModel);
+            var result = await context.VehicleModels.FirstOrDefaultAsync(v => v.ID == ID);
+            if (result != null)
+            {
+               context.VehicleModels.Remove(result);
+                await context.SaveChangesAsync();
+
+            }
+            return result;
         }
-         public void UpdateVehicleModel(VehicleModel vehicleModel)
+        public async Task<VehicleModel> UpdateVehicleModel(VehicleModel vehicleModel)
         {
-            context.Entry(vehicleModel).State=EntityState.Modified;
-        }
-        public void Save()
-        {
-            context.SaveChanges();
+            var result = await context.VehicleModels.FirstOrDefaultAsync(v => v.ID == vehicleModel.ID);
+            return result;
         }
 
+        public async Task<VehicleModel> GetVehicleModelByID(int? id)
+        {
+            return await context.VehicleModels.FindAsync(id);
+        }
     }
 }
